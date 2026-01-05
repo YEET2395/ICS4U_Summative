403b30d516d87a8515a769c1f906972979e2c424
Aadil Kureshy <aadil.kureshy91@gmail.com>
2026-01-05 14:29:50 -0500

    Changed role to int for easier comparison. Added a method to easily calculate the grid distance between the BaseBot and all other robots. Added a new class which tracks player information like id, role, name, coordinates, and hp and

--- BaseBot.java
+++ BaseBot.java
@@ -4,7 +4,7 @@ import becker.robots.*;
 
 public abstract class BaseBot extends RobotSE {
 
-    public final String role; // GUARD, VIP, CHASER
+    public final int role; // GUARD = 2, VIP = 1, CHASER = 3
     public final int id;
     public final int hp;
 
@@ -18,7 +18,7 @@ public abstract class BaseBot extends RobotSE {
      * @param id identifier of the bot
      * @param hp health points of the bot
      */
-    public BaseBot(City city, int str, int ave, Direction dir, String role, int id, int hp)
+    public BaseBot(City city, int str, int ave, Direction dir, int role, int id, int hp)
     {
         super(city, str, ave, dir);
         this.role = role;
@@ -33,6 +33,27 @@ public abstract class BaseBot extends RobotSE {
     public int[] getMyPosition()
     {
         return new int[] {this.getStreet(), this.getAvenue()};
+    }
+
+    /**
+     * Calculates the grid distance between the robot invoking the method and all other robots (including the invoker)
+     * @param records the array of other players
+     * @return the distances to each other robot
+     */
+    public int[] getDistances(playerInfo [] records) {
+        int [] gridDistance = new int[records.length-1];
+        int[] myCoords = this.getMyPosition();
+        int[] otherCoords;
+        //iterate through list of players (excludes the robot calling it using ID)
+        //and calculates the distance to get to each one
+        for (int i=0; i<records.length; i++) {
+            otherCoords = records[i].getPosition();
+            //check that the robot's position being compared is not the BaseBot invoking the method
+            if (this.id != records[i].getID()) {
+                gridDistance[i] = Math.abs(myCoords[0] - otherCoords[0]) + Math.abs(myCoords[1] - otherCoords[1]);
+            }
+        }
+        return gridDistance;
     }
 
     /**
---
+++ playerInfo.java
@@ -0,0 +1,87 @@
+package ICS4U_Summative;
+
+/**
+ * Application controlled records tracking each robot
+ * @author Aadil Kureshy, Austin Xiong,  Xinran Li
+ * @version 2026 1 5
+ */
+public class playerInfo {
+    private final int ID;
+    private int role;
+    private int hp;
+    private final int dodgeDifficulty;
+    private int[] position;
+
+    /**
+     * Constructor for playerInfo
+     * @param ID the numerical ID of the robot
+     * @param role the role of the robot
+     * @param hp the health of the robot
+     * @param dodgeDifficulty the dodge/catch capability of the robot (higher is better)
+     * @param pos the coordinates of the robot
+     */
+    public playerInfo (int ID, int role, int hp, int dodgeDifficulty, int[] pos) {
+        this.ID = ID;
+        this.role = role;
+        this.hp = hp;
+        this.dodgeDifficulty = dodgeDifficulty;
+        this.position = pos;
+    }
+
+    /**
+     * Get the ID number of the robot
+     * @return the ID number of the robot
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * Get the role of the robot
+     * @return the role of the robot
+     */
+    public int getRole() {
+        return role;
+    }
+
+    /**
+     * Changes the role after the robot runs out of HP
+     * @param role the role of the robot
+     */
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    /**
+     * Get the health of the robot
+     * @return the health of the robot
+     */
+    public int getHP() {
+        return hp;
+    }
+
+    /**
+     * Reduces the health of the robot by 1
+     */
+    public void reduceHP() {
+        if (this.hp>0) {
+            this.hp--;
+        }
+    }
+
+    /**
+     * The catch/dodge capability of the robot (higher is better)
+     * @return the dodge capability of the robot
+     */
+    public int getDodgeDifficulty() {
+        return dodgeDifficulty;
+    }
+
+    /**
+     * Get the position of the robot
+     * @return the coordinates of the robot
+     */
+    public int[] getPosition() {
+        return position;
+    }
+}
