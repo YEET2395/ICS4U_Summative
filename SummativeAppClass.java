cede2b335ca08d465a999409ed56f749dbd031e4
Aadil Kureshy <aadil.kureshy91@gmail.com>
2026-01-07 19:43:04 -0500

    Commented out the methods associated with the olf system of record distribution. Added two methods in the application class to get locations and hp based on role. Updated KureshyBot's logic

--- SummativeAppClass.java
+++ SummativeAppClass.java
@@ -30,54 +30,94 @@ public class SummativeAppClass {
         }
     }
 
+//    /**
+//     * Updates the robots copy of their records
+//     * @param array the array of BaseBots
+//     * @param publicRecords the records containing the info for the Guards/VIP
+//     * @param chaserRecords the records containing the info for the Chasers
+//     */
+//    public void updateBotRecords(BaseBot[] array, playerInfo[] publicRecords, playerInfo[] chaserRecords) {
+//        //iterate through the robot array
+//        for (int i=0; i<array.length; i++) {
+//
+//            //check if the robot is a VIP/Guard; otherwise give chaser records
+//            if (array[i].getMyRole() != 3) {
+//                array[i].setRecords(publicRecords);
+//            } else {
+//                array[i].setRecords(chaserRecords);
+//            }
+//
+//        }
+//    }
+
     /**
-     * Updates the robots copy of their records
-     * @param array the array of BaseBots
-     * @param publicRecords the records containing the info for the Guards/VIP
-     * @param chaserRecords the records containing the info for the Chasers
-     */
-    public void updateBotRecords(BaseBot[] array, playerInfo[] publicRecords, playerInfo[] chaserRecords) {
-        //iterate through the robot array
-        for (int i=0; i<array.length; i++) {
-
-            //check if the robot is a VIP/Guard; otherwise give chaser records
-            if (array[i].getMyRole() != 3) {
-                array[i].setRecords(publicRecords);
-            } else {
-                array[i].setRecords(chaserRecords);
-            }
-
-        }
-    }
-
-    /**
-     * Updates the records used by the application/robots
+     * Updates the records used by the application
      * @param array the array of BaseBots
      * @param records the array of records to update
-     * @param isChaserRecords to check whether to update the records with health values or not
      */
-    public void updateRecords(BaseBot[] array, playerInfo[] records, boolean isChaserRecords) {
+    public void updateRecords(BaseBot[] array, playerInfo[] records) {
+
         //iterate through the list of records (each record should match with the BaseBot in array)
         for (int i=0; i<records.length; i++) {
 
-            //checks whether it's updating the application/public record or chaser record
-            if (!isChaserRecords) {
-                records[i].updateRecords(array[i].getMyHP(), array[i].getMyPosition(), array[i].getMyState());
-            } else {
-                records[i].updateRecords(array[i].getMyPosition(), array[i].getMyState());
+            //update the info that will change
+            records[i].updateRecords(array[i].getMyHP(), array[i].getMyPosition(), array[i].getMyState());
             }
 
         }
+
+    /**
+     * Gets the position of all robots of a specific role from the records
+     * @param records the application records
+     * @param numRobots the number of robots of that role
+     * @param role the role of desired robots
+     * @return the x,y coordinates of the robots of the specified role
+     */
+    public static int[][] getPosOfRole(playerInfo[] records, int numRobots, int role) {
+        int[][] robotPos = new int[numRobots][2];
+        int count = 0; //robotPos index
+
+        //iterate through records
+        for (int i=0; i<records.length; i++) {
+
+            //checks role
+            if (records[i].getRole() == role) {
+                robotPos[count] = records[i].getPosition();
+                count++;
+            }
+
+        }
+
+        return robotPos;
+    }
+
+    /**
+     * Gets the HP of all robots of a role from the records
+     * @param records the application records
+     * @param numRobots the number of robots of that role
+     * @param role the role of desired robots
+     * @return the HP of the robots of the specified role
+     */
+    public static int[] getHPOfRole(playerInfo[] records, int numRobots, int role) {
+        int[] robotHP = new int[numRobots];
+        int count = 0; //robotHP index
+
+        //iterate through records
+        for (int i=0; i<records.length; i++) {
+
+            //checks role
+            if (records[i].getRole() == role) {
+                robotHP[count] = records[i].getHP();
+                count++;
+            }
+        }
+
+        return robotHP;
     }
 
     public static void main(String[] args){
         setupPlayground();
 
         playerInfo[] appRecords;
-        playerInfo[] publicRecords;
-        playerInfo[] chaserRecords;
-
-
-
     }
 }
